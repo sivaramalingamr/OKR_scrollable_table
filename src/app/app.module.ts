d0164ceb279e965ef5c0c567f9cf92a5e3741fcb
sivaramalingamr <71115955+sivaramalingamr@users.noreply.github.com>
2021-06-30 19:54:13 +0530

    remove unrelated code
    remove unrelated code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,34 +4,17 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
-// import { DisplayComponent } from './components/display/display.component';
-// import { LoaderComponent } from './loader/loader.component';
-import { DesktopAppService } from './services/desktop-app.service';
-import { NgImageSliderModule } from 'ng-image-slider';
 import { HttpClientModule } from '@angular/common/http';
-import { GoogleMapsModule } from '@angular/google-maps';
-import { SwiperModule } from 'swiper/angular';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { NgxImageZoomModule } from 'ngx-image-zoom';
-import { SlickCarouselModule } from 'ngx-slick-carousel';
-import { SliderComponent } from './components/slider/slider.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent,
-    SliderComponent
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule, 
+    AppRoutingModule,
     HttpClientModule,
-    NgImageSliderModule,
-    GoogleMapsModule,
-    SwiperModule,
-    NgbModule,
-    NgxImageZoomModule,
-    SlickCarouselModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/services/desktop-app.service.ts
+++ src/app/services/desktop-app.service.ts
@@ -9,42 +9,8 @@ import { BehaviorSubject, Observable } from 'rxjs';
 export class DesktopAppService {
 
   constructor(private http: HttpClient) { }
-  srcURL = 'http://localhost:8000/';
-  authToken = '';
-  user$ = new BehaviorSubject(null);
-
-  getRemainderList(): Observable<any> {
-    return this.http.get(this.srcURL + 'users');
-  }
-
-  getProductsData(): Observable<any> {
-    return this.http.get(this.srcURL + 'products');
-  }
 
   getJsonData(): Observable<any> {
     return this.http.get('assets/json/test.json');
-  }
-
-  loginUser(loginFormObj: any): Observable<any> {
-    return this.http.post(this.srcURL + 'authuser', loginFormObj)
-  }
-
-  addProduct(postValues: any): Observable<any> {
-    return this.http.post(this.srcURL + 'addproduct', postValues);
-  }
-
-  addCategory(postValues: any): Observable<any> {
-    return this.http.post(this.srcURL + 'addcategory', postValues);
-  }
-
-  setUserSession(userObj: any): void {
-    this.user$.next(userObj);
-  }
-
-  setAuthToken(token: string): void {
-    this.authToken = token;
-  }
-  getAuthToken(): string {
-    return this.authToken;
   }
 }
